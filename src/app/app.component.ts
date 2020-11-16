ac1a470a1f573dceda5da1d35e82fe3dd485bdce
merlinpratheesh <merlinpratheesh@gmail.com>
2020-11-16 15:43:19 +0530

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import {FormBuilder} from '@angular/forms';
-
+import { FormArray } from '@angular/forms';
 import {Validators} from '@angular/forms';
 
 @Component({
@@ -13,11 +13,22 @@ export class AppComponent  {
    groupedform = this.fb.group({
    Name : ['', Validators.required],
    Email: [],
-   
+
+   knownlang: this.fb.array([
+    this.fb.control('')
+  ])
   });
   constructor(private fb:FormBuilder)
   {
 
+  }
+  get knownlanguagesfun()
+  {
+    return this.groupedform.get('knownlang') as FormArray;
+  }
+  setlangfun()
+  {
+    this.knownlanguagesfun.push(this.fb.control(''));
   }
   updateName(){
     
