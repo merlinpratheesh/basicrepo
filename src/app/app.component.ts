8895ea91f58a5d8ed8b8712cf40fb8956d9d3dc8
merlinpratheesh <merlinpratheesh@gmail.com>
2020-11-17 17:17:21 +0530

    worked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,8 @@
 import { Component } from '@angular/core';
-import {FormControl} from '@angular/forms';
-import {COMMA, ENTER} from '@angular/cdk/keycodes';
-import {MatChipInputEvent} from '@angular/material/chips';
 
 
 
-export interface Fruit {
-  name: string;
-}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
