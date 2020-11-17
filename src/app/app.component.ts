c18a3dab5fb22db428e46bd44bc646aa1b083619
merlinpratheesh <merlinpratheesh@gmail.com>
2020-11-17 16:22:00 +0530

    worked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
-import {FormControl} from '@angular/forms';
+import { Component,AfterViewInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   myControl = new FormControl();
   options: string[] = ['One', 'Two', 'Three'];
+
+  record = { lock: true };
+  reactiveForm = new FormGroup({
+    formctrl: new FormControl(false)
+  });
+
+  constructor(private fb: FormBuilder) {
+    const variable = this.reactiveForm.get('formctrl'); // create a variable 
+    variable!.valueChanges.subscribe(value => console.log(value));
+  }
+  ngAfterViewInit(): void {
+
+
+  }
 }
