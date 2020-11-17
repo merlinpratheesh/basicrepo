677849ccc3f0141d4e24b8280bf016cd6c6ad903
merlinpratheesh <merlinpratheesh@gmail.com>
2020-11-17 17:50:18 +0530

    worked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import {FormControl} from '@angular/forms';
+import {MatDialog} from '@angular/material/dialog';
 
+import {DialogActionsExampleComponent} from './dialog-actions-example/dialog-actions-example.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,4 +11,8 @@ import {FormControl} from '@angular/forms';
 export class AppComponent {
   myControl = new FormControl();
   options: string[] = ['One', 'Two', 'Three'];
+  constructor(private _matDialog: MatDialog) {}
+  onClick() {
+    this._matDialog.open(DialogActionsExampleComponent);
+  }
 }
