ddcec05eb186102894537c34cb117f76362aeb4e
merlinpratheesh <merlinpratheesh@gmail.com>
2020-11-17 16:55:02 +0530

    Worked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,19 @@ import { Component } from '@angular/core';
 import {FormControl} from '@angular/forms';
 import {COMMA, ENTER} from '@angular/cdk/keycodes';
 import {MatChipInputEvent} from '@angular/material/chips';
+
+
+
+export interface Fruit {
+  name: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export interface Fruit {
-  name: string;
-}
 export class AppComponent {
-  myControl = new FormControl();
-  options: string[] = ['One', 'Two', 'Three'];
+
   visible = true;
   selectable = true;
   removable = true;
