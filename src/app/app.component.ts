8b8d8226ebbf354298aad52729fe4783ca3dd4a1
merlinpratheesh <merlinpratheesh@gmail.com>
2020-11-17 17:10:39 +0530

    Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,33 +15,4 @@ export interface Fruit {
 })
 export class AppComponent {
 
-  visible = true;
-  selectable = true;
-  removable = true;
-  addOnBlur = true;
-  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
-  fruits: Fruit[] = [{ name: "Lemon" }, { name: "Lime" }, { name: "Apple" }];
-
-  add(event: MatChipInputEvent): void {
-    const input = event.input;
-    const value = event.value;
-
-    // Add our fruit
-    if ((value || "").trim()) {
-      this.fruits.push({ name: value.trim() });
-    }
-
-    // Reset the input value
-    if (input) {
-      input.value = "";
-    }
-  }
-
-  remove(fruit: Fruit): void {
-    const index = this.fruits.indexOf(fruit);
-
-    if (index >= 0) {
-      this.fruits.splice(index, 1);
-    }
-  }
 }
