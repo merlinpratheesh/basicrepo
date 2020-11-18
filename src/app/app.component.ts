b1c8e73ed79f619605149b6e4cf89379fed0578c
merlinpratheesh <merlinpratheesh@gmail.com>
2020-11-18 11:28:01 +0530

    InitialCommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {FormControl} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators,FormArray, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +9,43 @@ import {FormControl} from '@angular/forms';
 export class AppComponent {
   myControl = new FormControl();
   options: string[] = ['One', 'Two', 'Three'];
+
+  profileForm = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: [''],
+    address: this.fb.group({
+      street: [''],
+      city: [''],
+      state: [''],
+      zip: ['']
+    }),
+    aliases: this.fb.array([
+      this.fb.control('')
+    ])
+  });
+
+  get aliases() {
+    return this.profileForm.get('aliases') as FormArray;
+  }
+
+  constructor(private fb: FormBuilder) { }
+
+
+  updateProfile() {
+    this.profileForm.patchValue({
+      firstName: 'Nancy',
+      address: {
+        street: '123 Drew Street'
+      }
+    });
+  }
+
+  addAlias() {
+    this.aliases.push(this.fb.control(''));
+  }
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.profileForm.value);
+  }
 }
